e49a85d1e01bc9dcb6c7b426c3fb2ed8574c5415
crackhead6474 <abdullahsheikh6474760@gmail.com>
2026-04-04 21:11:23 +0600

    added error handling config

---
+++ src/main/java/com/project/boimela/BoimelaApplication.java
@@ -0,0 +1,12 @@
+package com.project.boimela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoimelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoimelaApplication.class, args);
+	}
+}
